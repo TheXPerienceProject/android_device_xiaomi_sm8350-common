8b6a76137d796123043e26aa6939874c22548116
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-01-31 09:53:11 +0530

    sm8350-common: Patch libwa_sat to load libion from vendor
    This seems to be used by stock camera.
    
    01-31 09:29:05.661   895 17066 E linker  : library "/system/lib64/libion.so" ("/system/lib64/libion.so") needed or dlopened by "/vendor/lib64/libwa_sat.so" is not accessible for the namespace: [name="vndk", ld_library_paths="", default_library_paths="/apex/com.android.vndk.v33/lib64:/vendor/lib64", permitted_paths="/odm/lib64/hw:/odm/lib64/egl:/vendor/lib64/hw:/vendor/lib64/egl:/system/vendor/lib64/hw:/system/vendor/lib64/egl:/apex/com.android.vndk.v33/lib64/hw"]
    01-31 09:29:05.661   895 17066 E walog/IonImageAlloc: Error dlopen in Library: /system/lib64/libion.so
    01-31 09:29:05.661   895 17066 E walog/IonImageAlloc: Failed to open ion device
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,9 @@ function blob_fixup() {
         vendor/lib64/android.hardware.secure_element@1.0-impl.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+        vendor/lib64/libwa_sat.so)
+            sed -i 's/\/system\/lib64\([^\/]\)/\/vendor\/lib64\1/g' "${2}"
+            ;;
     esac
 }
 
