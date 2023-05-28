3bb97956aebdfba60cc4c4582d7924e9c7de6341
klozz <carlosj@klozz.dev>
2023-05-28 06:48:36 +0000

    sm8350-common: build telephony from common
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -395,7 +395,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 TARGET_COMMON_QTI_COMPONENTS := \
-    perf
+    perf \
+    telephony
 
 # Power
 PRODUCT_PACKAGES += \
