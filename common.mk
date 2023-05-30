d4e015af643219ca626d9a8b2df2eec44302e038
Omkar Chandorkar <gotenksIN@aospa.co>
2023-05-30 14:47:49 +0900

    vili:  Disable getVsyncPeriodFromHWC
    - 5.4 display HAL seems to have some issues with newer qssi
      causing it to render UI in 60fps with 120Hz refresh rate in edge cases
    - it might be fixed with newer sdm/display-drivers, but this should fix it for now
    
    Change-Id: Idd4e054ac76b14d9d6ee8ead090231ab58f009b8
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -179,6 +179,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.use_latest_hwc_vsync_period=0
+
 # Dolby
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
