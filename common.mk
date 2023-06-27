b7a72bdd98c7b49129dc1bcf1dd508a6a18e900e
klozz <carlosj@klozz.dev>
2023-06-27 04:21:51 +0000

    sm8350-common:build usb from common
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -447,6 +447,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     qseecomd \
     telephony \
+    usb \
     wfd
 
 PRODUCT_COPY_FILES += \
@@ -558,16 +559,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
     android.hardware.usb@1.3 \
     android.hardware.usb@1.3.vendor
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \
