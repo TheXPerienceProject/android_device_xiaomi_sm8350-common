63c87a4b35f2ec7957f256cb51844d00f193f7a9
klozz <carlosj@klozz.dev>
2024-02-13 03:12:11 +0000

    sm8350-common: build vendor.qti.hardware.capabilityconfigstore@1.0.vendor
     this fix: 03-18 03:56:18.747  1887  1887 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.hardware.capabilityconfigstore@1.0-service": library "vendor.qti.hardware.capabilityconfigstore@1.0.so" not found: needed by main executable
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -169,6 +169,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Capabilityconfigstore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
