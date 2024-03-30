742d50cb1b563a42e317e995020cb01fa9987a40
Michael Bestas <mkbestas@gmail.com>
2024-03-29 18:57:34 -0600

    sm8350-common: dolby: Convert to SwitchPreferenceCompat
    Signed-off-by: klozz <carlosj@klozz.dev>
    Signed-off-by: jessepincay <pincayjesse@gmail.com>

--- dolby/src/com/xiaomi/dolby/DolbySettingsFragment.java
+++ dolby/src/com/xiaomi/dolby/DolbySettingsFragment.java
@@ -31,7 +31,7 @@ import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settingslib.widget.MainSwitchPreference;
 
