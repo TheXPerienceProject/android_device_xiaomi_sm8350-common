b820bc1ef5b4d99cd6fc736c1b9eecbf74f9f724
klozz <carlosj@klozz.dev>
2024-04-17 19:26:45 +0000

    sm8350-common:patch dolbycodec2 the patch for this was wrong and was patching displayfeature XD
     we dont have that
    
    Signed-off-by: jessepincay <pincayjesse@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -102,7 +102,7 @@ function blob_fixup() {
         vendor/lib64/libwa_sat.so)
             sed -i 's/\/system\/lib64\([^\/]\)/\/vendor\/lib64\1/g' "${2}"
             ;;
-        vendor/lib64/hw/displayfeature.default.so)
+        vendor/bin/hw/dolbycodec2)
             "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
