785e02e3073e7997467415ea15dc06d34eca647c
klozz <carlosj@klozz.dev>
2024-05-04 02:09:51 +0000

    sm8350-common: inherit vendor_framework_compatibility_matrix from core-utils
    Signed-off-by: jessepincay <pincayjesse@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,7 +77,8 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
-    $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml
+    $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
