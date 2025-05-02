7024dc8b97c3f4fc3823c06ba14f22761a30ea42
klozz <carlosj@klozz.dev>
2025-05-02 00:32:05 -0600

    sm8350-common: build with Yuki clang 21
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,7 +95,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := TARGET_PRODUCT=$(PRODUCT_DEVICE)
 #TARGET_KERNEL_NO_GCC := true
 #TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-yuki
 TARGET_KERNEL_LLVM_BINUTILS := true
-TARGET_KERNEL_CLANG_VERSION := 20
+TARGET_KERNEL_CLANG_VERSION := 21
 KERNEL_CC := CC=clang
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/debugfs.config vendor/xiaomi_QGKI.config
