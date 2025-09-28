aa0dff567b137f784e383cd5cb5e3314f8774831
klozz <carlosj@klozz.dev>
2025-09-27 22:09:41 -0600

    Revert "sm8350-common: Disable BT audio AIDL is broken on lahaina"
    we are on aosp now
    This reverts commit a4105118486a8c725e94f06237607fd474406f61.

--- common.mk
+++ common.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-service \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.audio-V3-ndk.vendor \
     android.hardware.bluetooth.a2dp@1.0 \
