327eb45253ccbb22378100dece62bdd296a11446
klozz <carlosj@klozz.dev>
2025-09-30 01:25:48 -0600

    Revert "sm8350-common: build again with clang21"
    This reverts commit 8c5060ddd5a14f26fd508aad31061226efcd9fff.
    
    Change-Id: I36e6ae085a748f554cb18f1e217ae84360010871
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -561,9 +561,11 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
+WITH_LINEAGE_POWER_SERVICE := true
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
