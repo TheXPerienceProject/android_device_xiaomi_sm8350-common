8986143ded00077dc7823986e6c7ca40b1f85be6
klozz <carlosj@klozz.dev>
2026-05-15 02:51:53 -0600

    sm8350-common: cleanup duplicated powerhint import
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -600,9 +600,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Preopt critical applications
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
